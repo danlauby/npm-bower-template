02f77e5059c99c43f34e06d7c6468fbcf702d8c5
Dan Lauby and Dylan Stackhouse <dylan@>
2017-03-14 11:50:22 -0700

    Hardcode target time for alarm

--- js/clock-interface.js
+++ js/clock-interface.js
@@ -1,7 +1,12 @@
 var Clock = require("./../js/clock.js").clockModule;
 $(function() {
+  var target = "Tuesday, March 14, 2017 11:49 AM";
   setInterval(function(){
     var clock = moment().format('LLLL');
+    if(clock === target){
+      $("body").css({"background-color":"#000"})
+    }
+
     $('.clock').text(clock);
   }, 500);
 });
